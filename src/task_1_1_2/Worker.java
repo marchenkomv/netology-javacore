b5a42f406ab2cf3e6bfa6f9b61d467db2ef521e4
marchenkomv <70087091+marchenkomv@users.noreply.github.com>
2021-02-25 15:37:16 +1000

    Update Worker.java

--- src/task_1_1_2/Worker.java
+++ src/task_1_1_2/Worker.java
@@ -13,7 +13,6 @@ public class Worker {
         for (int i = 0; i < 100; i++) {
             if (i != 33) {
                 callback.onDone("Task " + i + " is done");
-
             } else {
                 errorCallback.onError("Error on task " + i);
             }
